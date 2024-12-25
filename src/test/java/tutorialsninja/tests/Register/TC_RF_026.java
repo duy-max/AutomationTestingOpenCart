b3084ad5b246bfb09e09f8c83ba110868ae70f3d
TruongDu7 <61675298+duy-max@users.noreply.github.com>
2024-12-25 20:39:23 +0700

    complete all TC_RF without framework

---
+++ src/test/java/tutorialsninja/tests/Register/TC_RF_026.java
@@ -0,0 +1,44 @@
+package tutorialsninja.tests.Register;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utils.CommonUtils;
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+public class TC_RF_026 {
+    @Test
+    public void verifyUIOfRegisterAccountPage() throws IOException {
+
+        // https://drive.google.com/file/d/1X6EPJW-Ojl3Xpv99qrnOV4wU8FuekmtO/view
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.get("https://tutorialsninja.com/demo/");
+
+        driver.findElement(By.xpath("//span[text()='My Account']")).click();
+        driver.findElement(By.linkText("Register")).click();
+
+        TakesScreenshot ts = (TakesScreenshot)driver;
+        File srcScreenshot = ts.getScreenshotAs(OutputType.FILE);
+
+        try {
+            FileHandler.copy(srcScreenshot,new File(System.getProperty("user.dir")+"\\Screenshots\\actualRegisterPageUI.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Assert.assertTrue(CommonUtils.compareImages(System.getProperty("user.dir")+"\\Screenshots\\actualRegisterPageUI.png", System.getProperty("user.dir")+"\\Screenshots\\expectedRegisterPageUI.png"));
+
+        driver.quit();
+
+    }
+}
